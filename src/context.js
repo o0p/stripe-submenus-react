a7ba467d302e933143d154c5495c9c63dd3964cd
Ruslan <rus@gmx.com>
2021-06-01 22:51:42 +0100

    Add add isSidebarOpen state

--- src/context.js
+++ src/context.js
@@ -5,5 +5,6 @@ import sublinks from './data';
 const AppContext = React.createContext();
 
 const AppProvide = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   return <App.AppContext>{children}</App.AppContext>;
 };
