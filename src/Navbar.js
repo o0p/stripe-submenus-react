3649053ec9bce02a193b1bea13e956e55510d3ae
Ruslan <rus@gmx.com>
2021-06-02 10:51:21 +0100

    Add navbar markup

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,19 @@ import { useGlobalContext } from './context';
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
-  return <h2>navbar component</h2>;
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="stripe" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links"></ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
