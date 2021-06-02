a0ae176741dea3fbb99352cec724342369baa41b
Ruslan <rus@gmx.com>
2021-06-02 12:11:00 +0100

    Add hero markup

--- src/Hero.js
+++ src/Hero.js
@@ -3,9 +3,19 @@ import phoneImg from './images/phone.svg';
 import { useGlobalContext } from './context';
 
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>;
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments instastrucutre for the internet</h1>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} alt="phpne" className="phone-img" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
