e4a812ebef8d153ee95a0a0680d81c5f4b77b045
Ruslan <rus@gmx.com>
2021-06-02 12:12:35 +0100

    Add header and p

--- src/Hero.js
+++ src/Hero.js
@@ -9,6 +9,13 @@ const Hero = () => {
       <div className="hero-center">
         <article className="hero-info">
           <h1>Payments instastrucutre for the internet</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non porro
+            pariatur nihil animi tempore rem aut est nesciunt excepturi vero
+            cum, fugiat perferendis voluptatem temporibus veritatis iusto sint
+            sed dolorem veniam sequi quas, quaerat eveniet quidem.
+          </p>
+          <button className="btn">Start Now</button>
         </article>
         <article className="hero-images">
           <img src={phoneImg} alt="phpne" className="phone-img" />
