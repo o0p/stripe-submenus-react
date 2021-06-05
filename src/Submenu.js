07878c17ea65dad21cc602fbcb6827b220c492c0
Ruslan <rus@gmx.com>
2021-06-05 16:16:56 +0100

    Fix error

--- src/Submenu.js
+++ src/Submenu.js
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { useGlobalContext } from './context';
 
 const Submenu = () => {
-  const [isSubmenuOpen] = useGlobalContext();
+  const { isSubmenuOpen } = useGlobalContext();
   return <h2>submenu component</h2>;
 };
 
