b1857f901b1b66ad92567f9c52182b241f5d2997
Ruslan <rus@gmx.com>
2021-06-07 10:18:57 +0100

    Add columns state

--- src/Submenu.js
+++ src/Submenu.js
@@ -8,7 +8,9 @@ const Submenu = () => {
     page: { page, links },
   } = useGlobalContext();
   const container = useRef(null);
+  const [columns, setColumns] = useState('col-2');
   useEffect(() => {
+    setColumns('col-2');
     const submenu = container.current;
     const { center, bottom } = location;
     submenu.style.left = `${center}px`;
@@ -19,7 +21,7 @@ const Submenu = () => {
       className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
       ref={container}>
       <h4>{page}</h4>
-      <div className={`submenu-center col-2`}>
+      <div className={`submenu-center ${columns}`}>
         {links.map((link, index) => {
           const { label, icon, url } = link;
           return (
