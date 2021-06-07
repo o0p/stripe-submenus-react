f2ca363a0d1e7def97e9f6589d9aacdc0d49e435
Ruslan <rus@gmx.com>
2021-06-07 10:39:56 +0100

    Add content text and close submenu btn

--- src/Hero.js
+++ src/Hero.js
@@ -8,17 +8,19 @@ const Hero = () => {
     <section className="hero" onMouseOver={closeSubmenu}>
       <div className="hero-center">
         <article className="hero-info">
-          <h1>Payments instastrucutre for the internet</h1>
+          <h1>
+            Payments infrastructure <br />
+            for the internet
+          </h1>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non porro
-            pariatur nihil animi tempore rem aut est nesciunt excepturi vero
-            cum, fugiat perferendis voluptatem temporibus veritatis iusto sint
-            sed dolorem veniam sequi quas, quaerat eveniet quidem.
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
           </p>
-          <button className="btn">Start Now</button>
+          <button className="btn">Start now</button>
         </article>
         <article className="hero-images">
-          <img src={phoneImg} alt="phpne" className="phone-img" />
+          <img src={phoneImg} className="phone-img" alt="phone" />
         </article>
       </div>
     </section>
